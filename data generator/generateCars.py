9c74da58c425b48be050a49d430bc037df7acfe3
Yossi <yosef.malka@gmail.com>
2022-06-07 16:58:54 +0300

    add colors data

--- data generator/generateCars.py
+++ data generator/generateCars.py
@@ -6,8 +6,10 @@ rand = random.SystemRandom()
 
 branches = []
 garages = []
+colors = []
 
-# argv[1] - amount, argv[2] = not active ratio, argv[3] = models path, argv[4] = garages path, argv[5] = branches path.
+# argv[1] - amount, argv[2] = not active ratio, argv[3] = models path, argv[4] = garages path, argv[5] = branches path, argv[6] = colors path.
+
 
 def loadModels(path):
     results = []
@@ -22,8 +24,21 @@ def loadModels(path):
 def random_branch():
     return rand.choice(branches)
 
+
+def random_color():
+    rand = random.random()
+    for color in colors:
+        percent = float(color[2])/100
+        if rand < percent:
+            return int(color[0])
+        else:
+            rand -= percent
+    return int(colors[0][0])
+
+
 def random_garage():
     return rand.choice(garages)
+
 
 def generateData(amount, notActiveRatio, models):
     data = []
@@ -41,24 +56,34 @@ def generateData(amount, notActiveRatio, models):
         if not isActive:
             garage = random_garage()
         branch_id = random_branch()
-        data.append(f"{x},{int(licenseNumber)},{model},{year},{garage},{branch_id}\n")
+        color_id = random_color()
+        data.append(
+            f"{x},{int(licenseNumber)},{model},{year},{garage},{branch_id},{color_id}\n")
     return data
 
-def load_files(branches_path,garages_path):
+
+def load_files(branches_path, garages_path, colors_path):
     bfile = open(branches_path)
     blines = bfile.readlines()
     for b in blines:
         branches.append(int(b.split(',')[0][1:-1]))
     bfile.close()
-    gfile = open(garages_path,encoding="utf-8")
-    glines =gfile.readlines()
+    gfile = open(garages_path, encoding="utf-8")
+    glines = gfile.readlines()
     for g in glines:
         garages.append(int(g.split('\t')[0]))
+    gfile.close()
+    cfile = open(colors_path)
+    clines = cfile.readlines()
+    for c in clines:
+        colors.append(c.split(','))
+    cfile.close()
+
 
 def __main__():
     models = loadModels(argv[3])
-    load_files(argv[5],argv[4])
-    data = generateData(int(argv[1]),float(argv[2]), models)
+    load_files(argv[5], argv[4],argv[6])
+    data = generateData(int(argv[1]), float(argv[2]), models)
     results = open('results.csv', 'w')
     results.writelines(data)
     results.close()
